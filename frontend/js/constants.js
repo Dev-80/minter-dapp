d89f6f8228506c0ab8bcc2e064f60814317efa66
TL-C <marckuschua@gmail.com>
2022-08-22 11:52:43 +0800

    Update

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to Loveher.io";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'This is loveher.io. Beginning new thoughts and ideas to the NFT world.';
+const welcome_p = 'This is loveher.io. Bringing new thoughts and ideas to the NFT world.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
